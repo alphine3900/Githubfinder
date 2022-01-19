488edcd86383da58807db6772a31690d71e8d91b
alphine3900 <alphine.kirimi@student.moringaschool.com>
2022-01-19 08:37:22 +0300

    form error correction

--- src/app/form/form.component.ts
+++ src/app/form/form.component.ts
@@ -1,18 +1,4 @@
-// import { Component, OnInit } from '@angular/core';
 
-// @Component({
-//   selector: 'app-form',
-//   templateUrl: './form.component.html',
-//   styleUrls: ['./form.component.css']
-// })
-// export class FormComponent implements OnInit {
-
-//   constructor() { }
-
-//   ngOnInit(): void {
-//   }
-
-// }
 
 import { Component, OnInit, EventEmitter, Output } from '@angular/core';
 //  import { Searchservice } from ''; 
